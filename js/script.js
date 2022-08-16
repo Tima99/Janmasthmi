0a826659bc029e60747e8257389725842fc9fc06
AnkushRai <ankushrai-cse19@satyug.edu.in>
2022-08-16 10:54:34 +0530

    Music added

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
     
-const _name = document.querySelector('.name') 
+const _name = document.querySelector('.name')
 
-const _username = window.location.search.split('=')[1].replaceAll('_', '<br/>');
-_name.innerHTML = _username;
+const _username = window.location.search.split('=')[1] && window.location.search.split('=')[1].replaceAll('_', '<br/>');
+_name.innerHTML = _username || "नाम लिखें";
 
 function see(btn){
     const __name = btn.parentElement.querySelector('input').value
@@ -17,8 +17,31 @@ function see(btn){
 }
 
 function touch(ele){
+    music.song1.play();  
+
     ele.style.display = 'none'
     document.querySelector('.congrats-box').style.display ='flex'
     document.querySelector('.inputs-box').style.display ='flex'
 
 }
+
+// Music added
+
+var music = {
+    song1 : new Audio(), 
+    song2 : new Audio(),
+    song3 : new Audio()
+}
+music.song1.src = './sound/Krishna_Janmashtami.mp3'
+music.song2.src = './sound/happy_birthday2u.mp3'
+music.song3.src = './sound/Nanad_ke_Nanad_Bhayo.mp3'
+
+music.song1.addEventListener('ended', ()=>{
+    music.song2.play()
+})
+music.song2.addEventListener("ended", ()=>{
+    music.song3.play()
+})
+music.song3.addEventListener("ended", ()=>{
+    music.song1.play()
+})
